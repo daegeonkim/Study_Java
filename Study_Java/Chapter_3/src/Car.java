8d6370610df6a67cc943a676c3e98c207eb62a01
Dennis <daegeonkim@icloud.com>
2016-06-08 17:53:39 +0900

    Rest

---
+++ Study_Java/Chapter_3/src/Car.java
@@ -0,0 +1,17 @@
+
+public class Car {
+	public Car(){//<<생성자 		
+	}
+	int speed;
+	int distance;
+	String color;
+	public void speedUp(){
+		//내용생략
+	public void breakDown(){
+		//내용생략
+	}
+	}
+}
+
+// 무슨말인지 잘모르겠다 ..
+
