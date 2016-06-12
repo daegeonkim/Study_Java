8e3eb442fb60facb7219dcf0817d449e592a5df0
Dennis <daegeonkim@icloud.com>
2016-06-12 20:00:31 +0900

    기초문법 (1)

--- Study_Java/Chapter_5/src/Operators.java
+++ Study_Java/Chapter_5/src/Operators.java
@@ -1,28 +1,55 @@
 
 public class Operators {
+	int intValue1 = 5;
+	int intValue2= 10;
+	int result;
+	
 	public static void main(String args[]){
 		Operators sample = new Operators();
-		sample.only();
+		sample.complement();
 		
 	}
 	public void additive (){
-		int intValue1 = 5;
-		int intValue2= 10;
-		int result =intValue1 + intValue2;
+		result =intValue1 + intValue2;
 		System.out.println(result);
 		result = intValue2-intValue1;
 		System.out.println(result);	
 	}
 	public void only (){
-		int intValue1 = 5;
-		int intValue2= 10;
-		
-		int result = intValue1*intValue2;
+		result = intValue1*intValue2;
 		System.out.println(result);
 		result = intValue1/intValue2; // 0.5 지만 정수형으로 선언했기때문에 0으로 출력됨.
 		System.out.println(result);
 		result = intValue2/intValue1;
 		System.out.println(result);
 	}
+	public void remainder (){
+		int intValue1 = 53;
+		int intValue2 = 10;
+		result = intValue1%intValue2; // 나누기 하고 남은 나머지값을 구
+		System.out.println(result);
+	}
+
+	public void unary(){
+		int intValue = -10;
+		int intresult = +intValue; // +,- 로 만들어
+		System.out.println(intresult);
+		intresult = -intValue;
+		System.out.println(intresult);		
+	}
+	public void increDecre(){
+		int aa = 1;
+		System.out.println(aa++); // 출력 후 1을더함 그래서 출력값은 그대로 1
+		System.out.println(aa); //1이 더해졌기때문에  a값은 윗줄에서 2로 변경
+		System.out.println(++aa); // 1을 더한 후 출
+		System.out.println(aa); //그러므로 3 
+	}
+	public void complement(){
+		boolean flag = true;
+		System.out.println(flag); //!은boolean 값을 반대로 
+		System.out.println(!flag); //값반대로 후 출
+	}
+	.
+
 }
 
---
+++ Study_Java/Chapter_5/src/Operators_Test.java
@@ -0,0 +1,24 @@
+
+public class Operators_Test {
+	int intValue1 = 10;
+	public static void main (String[]args){
+		Operators_Test Test = new Operators_Test();
+		Test.compound();
+
+	}		
+	public void compound(){
+		intValue1 = intValue1 + 5;
+		System.out.println(intValue1);
+		intValue1 = intValue1 -5;
+		System.out.println(intValue1);
+		intValue1 = intValue1*5;
+		System.out.println(intValue1);
+		intValue1 = intValue1 /5;
+		System.out.println(intValue1);
+		intValue1 = intValue1 & 5 ;
+		System.out.println(intValue1);
+	}
+
+	p-
+	
+}
