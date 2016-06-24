8509ca12445ffe0276f0e79ed435072b4c50cb0b
Dennis <daegeonkim@icloud.com>
2016-06-24 17:51:09 +0900

    DTO Test

--- Study_Java/Chapter_8/src/Chapter_7/DTO_Test.java
+++ Study_Java/Chapter_8/src/Chapter_7/DTO_Test.java
@@ -1,5 +1,25 @@
 package Chapter_7;
 
 public class DTO_Test {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+	
+	
+	
+	public class MemberDTO {
+		String name; 
+		String phoneNumber;
+		String email;
+		
+	}
+	
+
+
+
 
 }
--- Study_Java/Chapter_8/src/Chapter_7/ReferenceTypes.java
+++ Study_Java/Chapter_8/src/Chapter_7/ReferenceTypes.java
@@ -7,27 +7,52 @@ public class ReferenceTypes {
 	public ReferenceTypes(String str){
 		
 	}
+	
+
+	
 	public static void main(String[] args) {
 		ReferenceTypes reference = new ReferenceTypes();
 		ReferenceTypes reference1 = new ReferenceTypes("a");
 	}
+	
+	
 	public class MemberDTO{ //뭐가다른걸까 DTO?? 패턴 ??
 		// DTO 어떤 속성을 갖는 클래스를 만들고 그 속성들을 쉽게 전달한다.
 		// 속성은 뭐고 전달은뭘
 		public String name;// 갑자기 왜 public 이 나온걸까 ..
-		public String phone;// 얘들은 왜 사용되지않았다는 내용이 나오지 않는걸까 ?
+		public String number;// 얘들은 왜 사용되지않았다는 내용이 나오지 않는걸까 ?
 		public String email; 
+		
+		public MemberDTO(){     //  DTO 생성자는 DTO 안에서 선언한
+		}
+		public MemberDTO(String name){
+		this.name = name;
+		}
+		public MemberDTO(String name, String number){
+			this.name = name;
+			this.number= number;
+		}
+		public MemberDTO(String name, String number, String email){
+			this.name = name;
+			this.number= number;
+			this.email = email;
+		}
+			
+	
 	}
+	
+	
+	
 	public MemberDTO getMemeberDTO(){
 		MemberDTO dto=new MemberDTO();
 		return	dto;
 	}
-	public void makeMemberDTO(){
+	
+	public void makeMemberObject(){
 		MemberDTO dto1 = new MemberDTO();
-		MemberDTO dto2 = new MemberDTO("snagmin");
-		MemberDTO dto3 = new MemberDTO("snagmin", "01066075086");
-		MemberDTO dto4 = new MemberDTO("sangmin", "01066075086", "sectoe999@gmail.com");
-	}
-
+		MemberDTO dto2 = new MemberDTO("Dennis");
+		MemberDTO dto3 = new MemberDTO("Dennis", "01066075086");
+		MemberDTO dto4 = new MemberDTO("Dennis", "01066075086", "Email");
+		}
 
 }
